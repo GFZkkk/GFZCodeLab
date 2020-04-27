835e4751706f8c311d59f6f296b728bf6c2ba811
高丰泽 <1424769011@qq.com>
2020-04-27 15:35:19 +0800

    优化代码

--- app/src/main/java/com/gfz/mvp/model/bean/BaseCalendarBean.kt
+++ app/src/main/java/com/gfz/mvp/model/bean/BaseCalendarBean.kt
@@ -4,15 +4,14 @@ package com.gfz.mvp.model.bean
  * created by gfz on 2020/4/6
  **/
 data class BaseCalendarBean(val date: IntArray) {
+
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (javaClass != other?.javaClass) return false
 
         other as BaseCalendarBean
 
-        if (!date.contentEquals(other.date)) return false
-
-        return true
+        return date.contentEquals(other.date)
     }
 
     override fun hashCode(): Int {
