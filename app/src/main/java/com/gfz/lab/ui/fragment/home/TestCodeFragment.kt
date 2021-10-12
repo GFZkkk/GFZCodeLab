b4b221b47c69238fc00fb693ee404302d1703e85
高丰泽 <1424769011@qq.com>
2021-10-12 16:59:12 +0800

    update kotlin coroutines version

--- app/src/main/java/com/gfz/lab/ui/fragment/home/TestCodeFragment.kt
+++ app/src/main/java/com/gfz/lab/ui/fragment/home/TestCodeFragment.kt
@@ -1,7 +1,7 @@
 package com.gfz.lab.ui.fragment.home
 
+import com.gfz.common.TimeCell
 import com.gfz.lab.databinding.FragmentCodeBinding
-import com.gfz.lab.ui.base.BaseFragment
 import com.gfz.lab.ui.base.BaseVBFragment
 
 /**
@@ -9,6 +9,10 @@ import com.gfz.lab.ui.base.BaseVBFragment
  * created by gaofengze on 2021/10/12
  */
 class TestCodeFragment: BaseVBFragment<FragmentCodeBinding>() {
+
+    val timeCell: TimeCell by lazy{
+        TimeCell()
+    }
 
     override fun initView() {
 
