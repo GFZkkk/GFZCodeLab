115c1527ac7bd9499fdf0fca76251678a2aabb14
xueya <2546258791@qq.com>
2022-02-10 11:05:39 +0800

    去掉 空数据隐藏view的逻辑

--- recyclerview/src/main/java/com/gfz/recyclerview/adapter/BaseRecyclerViewHolder.kt
+++ recyclerview/src/main/java/com/gfz/recyclerview/adapter/BaseRecyclerViewHolder.kt
@@ -15,7 +15,8 @@ import androidx.recyclerview.widget.RecyclerView
 
 abstract class BaseRecyclerViewHolder<T>(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-    var context: Context = itemView.context
+    val context: Context
+        get() = itemView.context
 
     private var listener: ((View, Int) -> Unit)? = null
 
@@ -24,41 +25,33 @@ abstract class BaseRecyclerViewHolder<T>(itemView: View) : RecyclerView.ViewHold
     }
 
     // region 加载方法
-    /**
-     * 空数据将不被显示
-     */
-    fun bindViewHolder(data: T?, position: Int) {
+    open fun initEvent() {}
 
+    fun bindViewHolder(data: T?, position: Int) {
         if (data != null) {
-            itemView.visibility = View.VISIBLE
             onBindViewHolder(data, position)
         } else {
-            itemView.visibility = View.INVISIBLE
             bindNoDataViewHolder()
         }
     }
 
-    /**
-     * 隐藏没有数据的viewholder
-     */
-    open fun bindNoDataViewHolder() {
+    protected abstract fun onBindViewHolder(data: T, position: Int)
 
-    }
+    protected open fun bindNoDataViewHolder() {}
+    // endregion
 
+    // region 对外方法
     open fun getHolderPosition(): Int {
         return bindingAdapterPosition
     }
 
-    open fun initEvent() {}
-
-    abstract fun onBindViewHolder(data: T, position: Int)
-    // endregion
-
     open fun setHolderListener(listener: ((View, Int) -> Unit)?) {
         this.listener = listener
     }
+    // endregion
 
-    protected open fun setHolderListener(vararg views: View) {
+    // region 工具方法
+    protected fun setViewOnClickListener(vararg views: View) {
         for (view in views) {
             view.setOnClickListener { v ->
                 click(v)
@@ -67,17 +60,16 @@ abstract class BaseRecyclerViewHolder<T>(itemView: View) : RecyclerView.ViewHold
     }
 
     //将点击事件传出去
-    protected open fun click(v: View) {
+    protected fun click(v: View) {
         listener?.let {
             val position = getHolderPosition()
-            if (position == -1){
+            if (position == -1) {
                 return
             }
             it.invoke(v, position)
         }
     }
 
-    // region 工具方法
     fun TextView.setTextColorRes(@ColorRes color: Int) {
         this.setTextColor(ContextCompat.getColor(context, color))
     }
