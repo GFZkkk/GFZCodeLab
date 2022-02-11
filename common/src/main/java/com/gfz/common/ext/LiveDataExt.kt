76c3bbead101d7d4dca61064279fd4ed2a3bbb97
xueya <2546258791@qq.com>
2022-02-11 11:25:44 +0800

    新增 livedata 扩展方法

---
+++ common/src/main/java/com/gfz/common/ext/LiveDataExt.kt
@@ -0,0 +1,33 @@
+package com.gfz.common.ext
+
+import android.os.Looper
+import androidx.lifecycle.MutableLiveData
+
+/**
+ * livedata
+ * created by gaofengze on 2022/2/10
+ */
+
+fun <T> MutableLiveData<T>.update(block: T.() -> Unit) {
+    // normal
+    val data = value?.apply(block)
+    if (Thread.currentThread() == Looper.getMainLooper().thread) {
+        value = data
+    } else {
+        postValue(data)
+    }
+    // sample
+    // value = value?.apply(block)
+}
+
+fun <T> MutableLiveData<List<T>>.updateList(block: MutableList<T>.() -> Unit) {
+    // normal
+    val data = value?.toMutableList()?.apply(block)
+    if (Thread.currentThread() == Looper.getMainLooper().thread) {
+        value = data
+    } else {
+        postValue(data)
+    }
+    // sample
+    // value = value?.toMutableList()?.apply(block)
+}
